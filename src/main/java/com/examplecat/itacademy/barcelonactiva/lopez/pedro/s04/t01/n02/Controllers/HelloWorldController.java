c5a74d5392348ea58f6923b9f05529f7452808bd
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-27 15:06:54 +0100

    updates methods String return;

--- src/main/java/com/examplecat/itacademy/barcelonactiva/lopez/pedro/s04/t01/n02/Controllers/HelloWorldController.java
+++ src/main/java/com/examplecat/itacademy/barcelonactiva/lopez/pedro/s04/t01/n02/Controllers/HelloWorldController.java
@@ -15,13 +15,13 @@ public class HelloWorldController {
 	@GetMapping("/HelloWorld")
 	@ResponseBody
 	public String hi(@RequestParam(name =("name"), defaultValue = "UNKNOWN") String name) {
-		return "Hi " + name + ", you are in";
+		return "Hi " + name + ", you are in Gradle.";
 	}
 	
 	@RequestMapping(value = {"/HelloWorld/", "/HelloWorld/{n}"})
 	@ResponseBody
 	public String hi2(@PathVariable(name= "n") Optional<String> optionalName) {
-		return (optionalName.isPresent())? "Hi " + optionalName.get() + ", you are in" : "Hi " + optionalName.orElse("UNKNOWN") + ", you are in";	
+		return (optionalName.isPresent())? "Hi " + optionalName.get() + ", you are in Gradle." : "Hi " + optionalName.orElse("UNKNOWN") + ", you are in Gradle.";	
 	}
 
 	
